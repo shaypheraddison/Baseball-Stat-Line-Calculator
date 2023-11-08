9a6d6ba3528cb4c07deba7429a0fb9cf8f1f6c8c
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-08 12:59:59 -0600

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-#This will show the batting average based on the information entered
-hits = input("How many hits?: ")
-at_bats = input("How many at-bats?: ")
-sum = int(hits) / int(at_bats)
-print("The batting average is: = {:.3f}".format(sum)) 
+#This will show the batting average based on the information entered. The goal is to get a whole slash line printed out.
+hit , ab = float(input("How many hits?: ")) , float(input("How many at-bats?: "))
+calc_avg = hit / ab
+print(f"The batting average is: = {calc_avg:.3f}")
+
+hbp , bb , sf = float(input("How many hit-by-pitches?:")) , float(input("How many walks?: ")) , float(input("How many sacrifice fly's?:"))
+calc_obp = (hbp + hit + bb) / (ab + hbp + bb + sf)
+print(f"The on-base percentage is: = {calc_obp:.3f}")
 
