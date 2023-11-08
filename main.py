3f11d1e28ddfdd0fd84fd41db0d69d018b85e0bf
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-08 14:06:24 -0600

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #This will show the batting average based on the information entered. The goal is to get a whole slash line printed out (avg/obp/slg/ops)
 hit , ab = float(input("How many hits?: ")) , float(input("How many at-bats?: "))
 calc_avg = hit / ab
-print(f"The batting average is: = {calc_avg:.3f}")
 
 hbp , bb , sf = float(input("How many hit-by-pitches?: ")) , float(input("How many walks?: ")) , float(input("How many sacrifice fly's?: "))
 calc_obp = (hbp + hit + bb) / (ab + hbp + bb + sf)
-print(f"The on-base percentage is: = {calc_obp:.3f}")
 
 single , double , triple , hr = float(input("How many singles?: ")) , float(input("How many doubles?: ")) , float(input("How many triples?: ")) , float(input("How many home runs?: "))
 calc_slg = (single + (double * 2) + (triple * 3) + (hr * 4)) / ab
-print(f"The slugging percentage is: = {calc_slg:.3f}")
 
 calc_ops = calc_obp + calc_slg
-print(f"The OPS is: = {calc_ops:.3f}")
+
+print(f"Here is the stat line for the batter: = {calc_avg:.3f}/{calc_obp:.3f}/{calc_slg:.3f}/{calc_ops:.3f}")
