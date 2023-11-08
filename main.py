f7a23104305d4891cae56f60fa0e2650ed13f3eb
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-08 10:22:24 -0600

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+#This will show the batting average based on the information entered
 hits = input("How many hits?: ")
 at_bats = input("How many at-bats?: ")
 sum = int(hits) / int(at_bats)
-print("The batting average is: = {:.3f}".format(sum))
+print("The batting average is: = {:.3f}".format(sum)) 
 
