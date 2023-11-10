14dfa526a30d23d633e8ac8d651af83f5e08c288
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-09 22:10:17 -0600

    Update Updated main.py
    I hope this isn't too excessive

--- Updated main.py
+++ Updated main.py
@@ -1,5 +1,30 @@
-def avg(hit , ab):
-    return hit/ab
-calc_avg = avg(float(input("How many hits?:")) , float(input("How many at-bats?: ")))
-print(f"The batting average is: = {calc_avg:.3f}")
+def get_input(stat):
+    return float(input("How many " + stat))
+
+hits = get_input("hits?: ")
+ab = get_input("at-bats?: ")
+bb = get_input("walks?: ")
+hbp = get_input("hit-by-pitches?: ")
+sac_fly = get_input("sacrifice fly's?: ")
+single = get_input("singles?: ")
+double = get_input("doubles?: ")
+triple = get_input("triples?: ")
+home_run = get_input("home runs?: ")
+
+def avg():
+    return hits/ab
+def obp():
+    return (hbp + hits + bb) / (ab + hbp + bb + sac_fly)
+def slg():
+    return (single + (double * 2) + (triple * 3) + (home_run * 4)) / ab
+def ops():
+    return obp() + slg()
+print(f"The batting average is: = {avg():.3f}")
+print(f"The on-base percentage is: = {obp():.3f}")
+print(f"The slugging percentage is: = {slg():.3f}")
+print(f"The on-base + slugging percentage is: = {ops():.3f}")
+print(f"The final slash line is: = {avg():.3f}/{obp():.3f}/{slg():.3f}/{ops():.3f}")
+
+
+
 
