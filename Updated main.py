97ca9be08b6313f3ab8cd0163bd0dd598dd933eb
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-14 15:00:39 -0600

    Update Updated main.py
    I....did it

--- Updated main.py
+++ Updated main.py
@@ -1,22 +1,59 @@
-stat_list = ["hits" , "at bats" , "walks" , "hit-by-pitches" , "sacrifice fly's" , "singles" , "doubles" , "triples" , "home runs"]
-stat = 0
-while stat < len(stat_list):
-    stat_input = input("How many " + stat_list[stat] + "?: ").split()
-    stat += 1
+def get_user_input():
+    stats = {}
+    stats["hits"] = int(input("Enter the number of hits: "))
+    stats["at_bats"] = int(input("Enter the number of at-bats: "))
+    stats["walks"] = int(input("Enter the number of walks: "))
+    stats["hit_by_pitch"] = int(input("Enter the number of hit-by-pitches: "))
+    stats["sacrifice_flies"] = int(input("Enter the number of sacrifice fly's: "))
+    stats["singles"] = int(input("Enter the number of singles: "))
+    stats["doubles"] = int(input("Enter the number of doubles: "))
+    stats["triples"] = int(input("Enter the number of triples: "))
+    stats["home_runs"] = int(input("Enter the number of home runs: "))
+    return stats
 
+def calculate_batting_average(hits, at_bats):
+    if at_bats == 0:
+        return 0.0
+    else:
+        return hits / at_bats
 
-def average():
-    return stat_list[0] / stat_list[1]
-def on_base_percent():
-    return stat_list[3] + stat_list[0] + stat_list[2] / stat_list[1] + stat_list[3] + stat_list[2] + stat_list[4]
-def slugging():
-    return stat_list[5] + (stat_list[6] * 2) + (stat_list[7] * 3) + (stat_list[8] * 4) / stat_list[1]
-def on_base_plus_slugg():
-    return on_base_percent() + slugging()
+def calculate_on_base_percentage(hits, walks, hit_by_pitch, at_bats, sacrifice_flies):
+    number = at_bats + walks + hit_by_pitch + sacrifice_flies
+    if number == 0:
+        return 0.0
+    else:
+        return (hits + walks + hit_by_pitch) / number
 
+def calculate_slugging_percentage(singles, doubles, triples, home_runs, at_bats):
+    total_bases = singles + 2 * doubles + 3 * triples + 4 * home_runs
+    if at_bats == 0:
+        return 0.0
+    else:
+        return total_bases / at_bats
 
-print(f"The final slash line is: {average():.3f}/{on_base_percent():.3f}/{slugging():.3f}/{on_base_percent():.3f}")
+def calculate_ops(hits, walks, hit_by_pitch, at_bats, sacrifice_flies, singles, doubles, triples, home_runs):
+    obp = calculate_on_base_percentage(hits, walks, hit_by_pitch, at_bats, sacrifice_flies)
+    slg = calculate_slugging_percentage(singles, doubles, triples, home_runs, at_bats)
+    return obp + slg
 
+def calculate_and_display_slash_line(stats):
+    batting_average = calculate_batting_average(stats["hits"], stats["at_bats"])
+    on_base_percentage = calculate_on_base_percentage(
+        stats["hits"], stats["walks"], stats["hit_by_pitch"], stats["at_bats"], stats["sacrifice_flies"]
+    )
+    slugging_percentage = calculate_slugging_percentage(
+        stats["singles"], stats["doubles"], stats["triples"], stats["home_runs"], stats["at_bats"]
+    )
+    ops = calculate_ops(
+        stats["hits"], stats["walks"], stats["hit_by_pitch"], stats["at_bats"],
+        stats["sacrifice_flies"], stats["singles"], stats["doubles"], stats["triples"], stats["home_runs"]
+    )
 
+    print(f"Batting Average: {batting_average:.3f}")
+    print(f"On-Base Percentage: {on_base_percentage:.3f}")
+    print(f"Slugging Percentage: {slugging_percentage:.3f}")
+    print(f"OPS: {ops:.3f}")
+    print(f"The final slash line is: {batting_average:.3f}/{on_base_percentage:.3f}/{slugging_percentage:.3f}/{ops:.3f}")
 
-
+user_stats = get_user_input()
+calculate_and_display_slash_line(user_stats)
